8421abf4303e35ee7842faa3a659cefc3b887c52
김민성 <kmseong0508@kyonggi.ac.kr>
2024-05-29 23:25:17 +0900

    sessionCheck 수정

--- src/utils/sessionCheck.js
+++ src/utils/sessionCheck.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const checkSession = async () => {
     try {
         const response = await axios.get('/api/check-session', { withCredentials: true });
-        return response.status === 200;
+        return response.data.statusCode === 200;
     } catch (error) {
         return false;
     }
