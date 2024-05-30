624d9834b91e7ef6eee0a7722bb9ace71dad3afe
김민성 <kmseong0508@kyonggi.ac.kr>
2024-05-30 22:39:03 +0900

    api 끗

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,7 @@ module.exports = defineConfig({
   transpileDependencies: true,
 
   //outputDir: "../spring_test/reforme/src/main/resources/static",
-  outputDir: "../dalle-image-generator/src/main/resources/static",
+  //C:\Users\rlaal\Desktop\temp\reforme (GPTapi)\reforme\src\main\resources\static
+  //outputDir: "../dalle-image-generator/src/main/resources/static",
+  outputDir: "../reforme (GPTapi)/reforme/src/main/resources/static",
 })
