7ae32fcfda26df4462d83e33418b1504e2a140b3
김민성 <kmseong0508@kyonggi.ac.kr>
2024-05-30 15:47:44 +0900

    세션 체킹 주석 처리

--- src/components/router/index.js
+++ src/components/router/index.js
@@ -25,21 +25,21 @@ const router = createRouter({
     routes,
 });
 
-router.beforeEach(async (to, from, next) => {
-    if (to.matched.some(record => record.meta.requiresAuth)) {
-        try {
-            const response = await axios.get('/api/check-session');
-            if (response.data.statusCode === 200) {
-                next(); // 세션이 유효한 경우 이동 허용
-            } else {
-                next({ name: 'signin_page' }); // 세션이 유효하지 않은 경우 로그인 페이지로 이동
-            }
-        } catch (error) {
-            next({ name: 'signin_page' }); // 오류 발생 시 로그인 페이지로 이동
-        }
-    } else {
-        next(); // requiresAuth가 없는 경우 이동 허용
-    }
-});
+// router.beforeEach(async (to, from, next) => {
+//     if (to.matched.some(record => record.meta.requiresAuth)) {
+//         try {
+//             const response = await axios.get('/api/check-session');
+//             if (response.data.statusCode === 200) {
+//                 next(); // 세션이 유효한 경우 이동 허용
+//             } else {
+//                 next({ name: 'signin_page' }); // 세션이 유효하지 않은 경우 로그인 페이지로 이동
+//             }
+//         } catch (error) {
+//             next({ name: 'signin_page' }); // 오류 발생 시 로그인 페이지로 이동
+//         }
+//     } else {
+//         next(); // requiresAuth가 없는 경우 이동 허용
+//     }
+// });
 
 export default router;
